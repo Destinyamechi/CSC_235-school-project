05e997bb434af00364a072b7d98dd5b2ccb7930d
Destinyamechi <80577704+Destinyamechi@users.noreply.github.com>
2022-11-05 20:54:28 +0100

    Create accept_names.c

---
+++ accept_names.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(){
+        char name[50];
+        for(int i = 0; i<10; i++){
+            printf("Please Enter a name:");
+            scanf("%s", &name);
+        }
+}
+
