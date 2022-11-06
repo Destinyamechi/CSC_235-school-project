e636edc0f96348970fafaf20cc4ff40a545a5207
Destinyamechi <80577704+Destinyamechi@users.noreply.github.com>
2022-11-06 16:34:00 +0100

    Update quadratic_equation.c

--- quadratic_equation.c
+++ quadratic_equation.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <math.h>
+#include <complex.h>
 int main(){
-        float  a,b,c,z,x1,x2;
+        complex double c1,c2;
+        double  a,b,c,z,x1,x2;
         printf("Enter the co-efficient of x^2: ");
-        scanf("%f", &a);
+        scanf("%2lf", &a);
         printf("\n Enter the co-efficient of x: ");
-        scanf("%f", &b);
+        scanf("%2lf", &b);
         printf("\n Enter final number: ");
-        scanf("%f", &c);
+        scanf("%2lf", &c);
         z = sqrt((pow(b, 2)-(4*a*c)));
         x1 = (-b + z)/(2*a);
         x2 = (-b - z)/(2*a);
-        printf("The first root =  %.2f", x1);
-        printf("\nThe second root=  %.2f", x2);
+        if (z ==  0){
+                printf("The equation has equal roots\n The first root =  %.2lf", x1);
+        }
+        else if (z < 0){
+                c1 = (-b +z )/(2*a);
+                c2 = (-b - z)/(2*a);
+                printf("The first complex root = %.2lf+%.2lfi\n", creal(c1), cimag(c1));
+                printf("The first complex root = %.2lf+%.2lfi\n", creal(c2), cimag(c2));
+        }
+        else{
+                printf("The first root =  %.2lf", x1);
+                printf("\nThe second root=  %.2lf", x2);
+        }
         return 0;
-
 }
